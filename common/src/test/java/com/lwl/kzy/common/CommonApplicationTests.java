e96a61bad8b035927b765202a66f711d30bc51a8
lwl <lanwenliang@yidianlife.com>
2020-12-14 11:51:34 +0800

    初始化

---
+++ common/src/test/java/com/lwl/kzy/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwl.kzy.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
